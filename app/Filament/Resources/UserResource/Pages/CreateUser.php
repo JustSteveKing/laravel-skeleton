f069b96d9d9bd8186ec106397ad347f653aab8e3
Steve McDougall <juststevemcd@gmail.com>
2023-11-10 23:23:12 +0000

    Adding Spatie CSP package

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
-class CreateUser extends CreateRecord
+final class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
 }
